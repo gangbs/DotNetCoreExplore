918df9075dced07d703165c7642f7a6145269c41
杨彦钢 <yangyangangil@hotmail.com>
2019-01-31 13:50:02 +0800

    创建.net core api demo项目

---
+++ DotNetCore/DotNetCore.Api/Helper/AppSettingJson.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCore.Api.Helper
+{
+    public class AppSettingJson
+    {
+        public static IConfiguration GetConfig(string path = "appsettings.json")
+        {
+            var builder = new ConfigurationBuilder().AddJsonFile(path);//能否不指定
+            var configuration = builder.Build();
+            return configuration;
+        }
+
+
+        public static string GetValue(string key)
+        {
+            var config = GetConfig();
+            return config[key];
+        }
+
+        public static T GetValue<T>(string key)
+        {
+            var config = GetConfig();
+            return config.GetValue<T>(key);
+        }
+
+        public static T GetModel<T>(string key) where T : class, new()
+        {
+            var config = GetConfig();
+            Type tp = typeof(T);
+            var obj = (T)tp.Assembly.CreateInstance(tp.FullName);
+            config.GetSection(key).Bind(obj);
+            return obj;
+        }
+    }
+}
